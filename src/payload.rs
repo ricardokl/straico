62235e5792d60e702c59350968a254fe61ba3aa2
Ricardo Kullock <ricardokullock@gmail.com>
2024-10-10 17:02:56 +0200

    refactor: Improve Payload struct and request method formatting and readability.

--- src/payload.rs
+++ src/payload.rs
@@ -1,20 +1,36 @@
-use std:: error::Error;
-use reqwest::{Client, header::AUTHORIZATION};
+use reqwest::Client;
 use serde::Serialize;
+use std::error::Error;
 
 use crate::api_response::ApiResponse;
 
 #[derive(Debug, Serialize)]
-pub struct Payload { models: Vec<String>, message: String }
+pub struct Payload {
+    models: Vec<String>,
+    message: String,
+}
 
 impl Payload {
     pub fn new(models: &Vec<String>, message: &String) -> Self {
-        Self { models: models.to_owned(), message: message.to_owned() }
+        Self {
+            models: models.to_owned(),
+            message: message.to_owned(),
+        }
     }
 
-    pub async fn request(&self, client: &Client, url: &str, api_key: &String) -> Result<ApiResponse, Box<dyn Error>> {
-        Ok(client.post(url)
-            .header(AUTHORIZATION, format!("Bearer {}", api_key))
-            .json(&self).send().await?.json::<ApiResponse>().await?)
-        }
+    pub async fn request(
+        &self,
+        client: &Client,
+        url: &str,
+        api_key: &String,
+    ) -> Result<ApiResponse, Box<dyn Error>> {
+        Ok(client
+            .post(url)
+            .bearer_auth(api_key)
+            .json(&self)
+            .send()
+            .await?
+            .json::<ApiResponse>()
+            .await?)
+    }
 }
